261924133312e94ff17080b8b601e71f4f8d6fdf
Ro11ingButler <rollingkai1214@gmail.com>
2023-12-15 22:18:48 +0800

    Configure tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        "data-theme": "forest",
+        class: "font-terraria",
+      },
+    },
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+});
